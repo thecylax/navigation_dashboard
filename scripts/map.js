e517f0534d899309f8a4d833ab633566b3e80b70
Robson Cardoso dos Santos <cardoso@localhost.localdomain>
2016-10-20 14:14:06 -0200

    Added satellite and terrain layer with control layer over map Code organization
    Signed-off-by: Robson Cardoso dos Santos <cardoso@localhost.localdomain>

--- scripts/map.js
+++ scripts/map.js
@@ -1,25 +1,44 @@
+// General purpose vars
 var count = 0;
 var markers = [];
 var totalDistance = 0;
+var centralLatLng = [-25.432, -49.247] // Curitiba
 
+// mapbox specific vars
+var mapboxToken = 'pk.eyJ1IjoiY3lsYXgiLCJhIjoiY2l1ZmYxdmdxMDBkbDJvcDRjYzg0eHN1NCJ9.0DSzwUnu3ew-okLTsg8VXA';
+var mapboxUrl1 = 'https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token={accessToken}';
+var mapboxUrl2 = 'https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v10/tiles/256/{z}/{x}/{y}?access_token={accessToken}';
+var mapboxAttribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>';
 
-var latlong = [-25.432, -49.247]
-var mymap = L.map('mapid').setView(latlong, 13);
-L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v10/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
-    accessToken: 'pk.eyJ1IjoiY3lsYXgiLCJhIjoiY2l1ZmYxdmdxMDBkbDJvcDRjYzg0eHN1NCJ9.0DSzwUnu3ew-okLTsg8VXA',
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-}).addTo(mymap);
+var terrain = L.tileLayer(mapboxUrl1, {accessToken: mapboxToken, attribution: mapboxAttribution}),
+    satellite = L.tileLayer(mapboxUrl2, {accessToken: mapboxToken, attribution: mapboxAttribution});
 
+var mymap = L.map('mapid', {
+    center: centralLatLng,
+    zoom: 13,
+    layers: [terrain, satellite]
+});
+var baseMaps = {
+    "Terreno": terrain,
+    "Satélite": satellite
+};
 
-// var circle = L.circle(latlong, {
+// var circle = L.circle(centralLatLng, {
 //     color: 'red',
 //     fillColor: '#f03',
 //     fillOpacity: 0.5,
 //     radius: 500
 // }).addTo(mymap);
 
+
+// Functions
+function updateMarkBadge(n) {
+    $('#markscount').html(n)
+}
+
 function onMapClick(e) {
     count++;
+    updateMarkBadge(count);
     markers.push(e.latlng);
     var coordinates = [e.latlng.lat, e.latlng.lng];
     L.marker(coordinates, {title: '#'+count}).addTo(mymap);
@@ -44,4 +63,8 @@ function setAllMap(map) {
 function clearMarkers() {
     setAllMap(null);
 }
+
+
+// Main
+L.control.layers(baseMaps).addTo(mymap);
 mymap.on('click', onMapClick);
