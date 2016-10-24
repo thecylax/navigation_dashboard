2b608bdbffddf36e85299d2f0eb4e22722d0b9d9
Robson Cardoso dos Santos <cardoso.rcs@gmail.com>
2016-10-24 10:30:27 -0200

    Made some visual cleanings.
    Signed-off-by: Robson Cardoso dos Santos <cardoso.rcs@gmail.com>

--- scripts/core.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    // Set some default text
-    $('#waypoints').val('wp\tlatitude\t\t\t\tlongitude\n');
-    $('#hb1').click(function() {
-	console.log('clicked');
-    });
-});
